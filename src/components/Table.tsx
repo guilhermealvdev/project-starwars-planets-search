033b042f6bdca55d39b6161cca6ea9ff1cda2bca
Guilherme Alvarez <guilherme.zalv@gmail.com>
2024-02-24 16:09:04 -0300

    Projeto Done

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,20 +1,100 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { PlanetsContext } from '../contexts/PlanetsContext';
 
 function Table() {
   const { planets } = useContext(PlanetsContext)!;
-  console.log(planets); // Array de objetos
   const [filter, setFilter] = useState('');
+  const [filters, setFilters] = useState([]);
+  const [availableColumns, setAvailableColumns] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+  const [selectedColumn, setSelectedColumn] = useState('population');
+  const [selectedComparison, setSelectedComparison] = useState('maior que');
+  const [filterValue, setFilterValue] = useState('0');
 
-  const filteredPlanets = planets.filter((planet) => {
-    return (
-      planet.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  });
+  useEffect(() => {
+    setFilter('');
+    setFilters([]);
+    setAvailableColumns([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+    setSelectedColumn('population');
+    setSelectedComparison('maior que');
+    setFilterValue('0');
+  }, []);
+
+  const filterPlanets = (planet) => {
+    return filters.every(({ columnName, comparador, num }) => {
+      const planetValue = parseFloat(planet[columnName]);
+      switch (comparador) {
+        case 'maior que':
+          return planetValue > num;
+        case 'menor que':
+          return planetValue < num;
+        case 'igual a':
+          return planetValue === num;
+        default:
+          return true;
+      }
+    });
+  };
 
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
+
+  const handleFilterNumerico = (e) => {
+    e.preventDefault();
+    const newFilter = {
+      columnName: selectedColumn,
+      comparador: selectedComparison,
+      num: parseFloat(filterValue),
+    };
+    setFilters([...filters, newFilter]); // Adiciona o novo filtro à lista de filtros existente
+    setSelectedColumn('population');
+    setSelectedComparison('maior que');
+    setFilterValue('0');
+
+    const updatedAvailableColumns = availableColumns.filter(
+      (column) => column !== newFilter.columnName,
+    );
+    setAvailableColumns(updatedAvailableColumns);
+  };
+
+  const removeFilter = (index) => {
+    const newFilters = [...filters];
+    const removedColumn = newFilters[index].columnName;
+    newFilters.splice(index, 1);
+    setFilters(newFilters);
+
+    setAvailableColumns([...availableColumns, removedColumn]);
+  };
+
+  const removeAllFilters = () => {
+    setFilters([]);
+    setAvailableColumns([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+  };
+
+  const filteredPlanets = planets.filter((planet) => {
+    return (
+      planet.name.toLowerCase().includes(filter.toLowerCase())
+      && filterPlanets(planet)
+    );
+  });
 
   return (
     <div>
@@ -22,11 +102,62 @@ function Table() {
         <input
           type="text"
           placeholder="Filtro de Texto"
-          data-testid="name-filter"
           value={ filter }
           onChange={ handleFilterChange }
+          data-testid="name-filter"
         />
       </div>
+      <div>
+        <form onSubmit={ handleFilterNumerico }>
+          <select
+            name="columnName"
+            value={ selectedColumn }
+            onChange={ (e) => setSelectedColumn(e.target.value) }
+            data-testid="column-filter"
+          >
+            {availableColumns.map((option) => (
+              <option key={ option } value={ option }>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name="comparador"
+            value={ selectedComparison }
+            onChange={ (e) => setSelectedComparison(e.target.value) }
+            data-testid="comparison-filter"
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+
+          <input
+            type="number"
+            name="num"
+            value={ filterValue }
+            onChange={ (e) => setFilterValue(e.target.value) }
+            data-testid="value-filter"
+          />
+
+          <button type="submit" data-testid="button-filter">Filtrar</button>
+        </form>
+      </div>
+      {filters.map((filter2, index) => (
+        <div key={ index } data-testid="filter">
+          <span>
+            Filtro:
+            {filter2.columnName}
+            {filter2.comparador}
+            {filter2.num}
+          </span>
+          <button onClick={ () => removeFilter(index) }>Remover</button>
+        </div>
+      ))}
+      <button onClick={ removeAllFilters } data-testid="button-remove-filters">
+        Remover todas filtragens
+      </button>
       <table>
         <thead>
           <tr>
@@ -45,8 +176,8 @@ function Table() {
             <th>URL</th>
           </tr>
         </thead>
-        <tbody>
-          {filteredPlanets.map((planet, index) => ( // .map no array de objetos para lista-los
+        <tbody data-testid="planet-table">
+          {filteredPlanets.map((planet, index) => (
             <tr key={ index }>
               <td>{planet.name}</td>
               <td>{planet.rotation_period}</td>
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
+import { render, fireEvent, screen } from '@testing-library/react';
+import App from '../App';
 
-test('I am your test', () => {
-  const I = "your father";
-  expect(I).toBe("your father");
+test('renders Star Wars Planets title', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Star Wars Planets/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('filters planets by name', () => {
+  render(<App />);
+  const input = screen.getByTestId('name-filter');
+  fireEvent.change(input, { target: { value: 'Tatooine' } });
+  expect(input.value).toBe('Tatooine');
+});
+
+test('clicking filter buttons', () => {
+  render(<App />);
+  
+  // Simulando a seleção de um filtro
+  fireEvent.change(screen.getByTestId('column-filter'), { target: { value: 'population' } });
+  fireEvent.change(screen.getByTestId('comparison-filter'), { target: { value: 'maior que' } });
+  fireEvent.change(screen.getByTestId('value-filter'), { target: { value: '1000000000' } });
+  fireEvent.click(screen.getByTestId('button-filter'));
+
+  // Verificando se o filtro foi adicionado corretamente
+  const filterElement = screen.getByTestId('filter');
+  expect(filterElement).toBeInTheDocument();
+
+  // Simulando a remoção do filtro
+  fireEvent.click(screen.getByTestId('button-remove-filters'));
+
+  // Verificando se o filtro foi removido corretamente
+  const removedFilterElement = screen.queryByTestId('filter');
+  expect(removedFilterElement).not.toBeInTheDocument();
 });
